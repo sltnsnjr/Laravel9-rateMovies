22d2c4c6f3a190ceba5bbf3fc61f7f04541195f8
sltnsnjr <sltnsnjr6@gmail.com>
2022-05-24 01:40:12 +0300

    changes added

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,21 +1,45 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+
+<--HEAD>
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
+
+    <title>@yield("title")</title>
+
+    <!-- Loading third party fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Roboto:300,400,700|" rel="stylesheet" type="text/css">
+    <link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Loading main css file -->
+    <link rel="stylesheet" href="assets/style.css">
+
+    <!--[if lt IE 9]>
+    <script src="js/ie-support/html5.js"></script>
+    <script src="js/ie-support/respond.js"></script>
+    <![endif]-->
+
+    @yield("head")
+
 </head>
+
 <body>
-<h1>Header</h1>
+@include("home.header")
 
 
 @section('sidebar')
-    This is the master sidebar.
+    @include("home.sidebar")
 @show
 
 <div class="container">
     @yield('content')
 </div>
 
-<h1>Footer</h1>
+@include("home.footer")
+
+
 @yield('foot')
 </body>
 </html>
